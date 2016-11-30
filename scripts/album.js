00cfcfbb87b065f10e05bd1f7ac63e724b1e63bf
bettinamaria <bettina_offenegger@hotmail.com>
2016-11-30 11:52:03 +0100

    Assignment 11 DomScriptAlbum View: 2nd attempt logged (toggle once in place but doesn't switch to 3rd album cover?

--- scripts/album.js
+++ scripts/album.js
@@ -58,14 +58,14 @@ var createSongRow = function(songNumber, songName, songLength) {
      return template;
  };
 
- var setCurrentAlbum = function(album) {
-     // #1
+// Set elements that we want to populate with text dynamically
      var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
      var albumImage = document.getElementsByClassName('album-cover-art')[0];
      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
+
+ var setCurrentAlbum = function(album) {
      // #2
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
@@ -82,13 +82,15 @@ var createSongRow = function(songNumber, songName, songLength) {
  };
  
  window.onload = function() {
-     setCurrentAlbum(allAlbums);
-     var allAlbums = document.getElementsByClassName('album-view-title')[0];
-     for (var i = 0; i < allAlbums.length; i++) {
-         allAlbums[i].addEventListener("click", function(event) {
-            allAlbums.toggle('clicked');
-        }); 
-     }
+     setCurrentAlbum(albumPicasso);
+     
+     var allAlbums = [albumMarconi, albumOdessa, albumPicasso];
+        
+     albumImage.addEventListener("click", function(event){
+         for (var i = 0; i < allAlbums.length; i++) {
+         setCurrentAlbum(allAlbums[i]).toggle('clicked');  
+         }
+     });
  };
 
 //Add an event listener to the album cover. When a user clicks it, the album page content should toggle between the three album objects: albumPicasso, albumMarconi, and your album object.//
