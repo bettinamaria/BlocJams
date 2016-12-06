8f58dfcf4a1ca34c1115276748edf6349a595dd9
bettinamaria <bettina_offenegger@hotmail.com>
2016-12-06 07:09:43 +0100

    Checkpoint 19 jQueryNextPreviousButtons: fixed updatePlayerBarSong function

--- scripts/album.js
+++ scripts/album.js
@@ -80,6 +80,14 @@ var createSongRow = function(songNumber, songName, songLength) {
      }
  };
 
+var updatePlayerBarSong = function() {
+
+    $('.currently-playing .song-name').text(currentSongFromAlbum.title);
+    $('.currently-playing .artist-name').text(currentAlbum.artist);
+    $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
+
+};
+
 var trackIndex = function(album, song) {
     return album.songs.indexOf(song);
 };
